46edd80793119bfec23661de31ac2c33e09536fc
Dkurunji <kurunjidhananjay7@gmail.com>
2021-08-20 18:08:49 +0530

    initial check

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import SignInPage from './pages/SignInPage';
+import {BrowserRouter, Switch,Route, Link} from "react-router-dom";
+import { Home } from './pages/Home';
+import HostelSelection from './pages/HostelSelection';
+import RoomSelection from './pages/RoomSelection';
+import NavBar from './components/NavBar';
+// import ConfirmationModal from './components/ConfirmationModal';
 
+export const HostelContext = React.createContext();
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <HostelContext.Provider value={{ isSigned: false, selectedBoys: true, booked:[], floor:1,}}>
+        {/* <div className="App">      
+            
+        
+       
+        </div> */}
+      
+        <NavBar/>
+        <Switch>             
+              <Route exact path="/home">
+                  <Home/>
+              </Route>                             
+              <Route exact path="/selectHostel">
+                  <HostelSelection />
+                </Route>                             
+              <Route exact path="/RoomSelection">
+                  <RoomSelection />
+                </Route>                             
+                <Route >
+                    <SignInPage/> 
+                </Route>                             
+        </Switch>
+      </HostelContext.Provider>
+      
   );
 }
 
